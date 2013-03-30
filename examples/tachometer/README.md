c5d3b731695297208df6dbdb2188dca4fd0b697b
Aaron Mavrinac <mavrinac@gmail.com>
2013-03-30 16:03:24 -0700

    Add first example, a graphical polling tachometer.

---
+++ examples/tachometer/README.md
@@ -0,0 +1,23 @@
+# Autogab Examples: Graphical Tachometer
+
+
+## Overview
+
+The tachometer example reads the engine RPM signal from OBD-II and displays it
+in a graphical tachometer readout.
+
+
+## Dependencies
+
+The tachometer example requires all of the base dependencies of Autogab, plus
+[PyGame] [pygame].
+
+
+## Graphics
+
+Tachometer vector graphics modified from originals by DragonArt, licensed
+under a Creative Commons Attribution-NonCommercial-ShareAlike [license] [cc].
+
+
+[pygame]: http://www.pygame.org
+[cc]: http://creativecommons.org/licenses/by-nc-sa/3.0/
---
+++ examples/tachometer/tachometer.py
@@ -0,0 +1,47 @@
+"""\
+OBD polling tachometer display.
+
+@author: Aaron Mavrinac
+@contact: mavrinac@gmail.com
+@license: GPL-3
+"""
+
+import argparse
+from math import sin, cos, radians
+import pygame as pg
+
+from autogab.obd import OBDConnectionContext
+
+
+def tachometer(self, device='/dev/ttyUSB0'):
+    with OBDConnectionContext(device=device) as obd:
+        while(True):
+            rpm_raw = obd.command('010C')
+            rpm_bytes = rpm_raw[0].split(' ')
+            a, b = [int(rpm_bytes[i], 16) for i in [2, 3]]
+            yield ((a * 256) + b) / 4.0
+
+
+def main(device):
+    pg.init()
+    screen = pg.display.set_mode((640, 480))
+    back = pg.image.load('tachometer_back.png')
+    center = pg.image.load('tachometer_center.png')
+    for rpm in tachometer(device):
+        angle = radians((rpm / 1000.0) * 45.0 - 126.0)
+        points = [(int(cos(angle) * x - sin(angle) * y) + 320,
+                   int(sin(angle) * x + cos(angle) * y) + 240) \
+                   for (x, y) in [(0, -160), (-10, 0), (10, 0)]]
+        screen.fill((0, 0, 0))
+        screen.blit(back, (80, 0, 480, 480))
+        pg.draw.polygon(screen, (255, 0, 0), points)
+        screen.blit(center, (288, 208, 64, 64))
+        pg.display.flip()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--device', dest='device', type=str,
+                        default='/dev/ttyUSB0')
+    args = parser.parse_args()
+    main(args.device)
